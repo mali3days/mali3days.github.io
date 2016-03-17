5ef0a279fe209c11b20d51d9a9fbb96cdc16112c
mali3days <mali3days@gmail.com>
2016-03-17 22:39:45 +0200

    Added files via upload

--- test/test.js
+++ test/test.js
@@ -1,31 +1,48 @@
-/**
- * Created by m3days on 13.03.16.
- */
-describe("isNumeric", function() {
-
-    describe("Возвращает true если аргумент число", function() {
-         function maketest(x) {
-            it("Все аргументы являются числами", function() {
-                assert(isNumeric(x));
-            });
-         }
-
-        for (var i = -5; i <= 5; i++ ) {
-            maketest(i);
-        }
+describe("catchError", function() {
+    it("если 1-й аргумент = 'undefined' возвращает true ", function() {
+        assert( catchError(undefined));
     });
 
-    it("Проверка ввода нечислового значения", function() {
-        assert.equal(isNumeric("abc"), false);
+    it("если 1-й аргумент = '' возвращает true ", function() {
+        assert.equal( catchError(""), true);
+    });
+
+    it("если 1-й аргумент = isNaN &&  typeof(1 аргумент) !== 'string' возвращает true ", function() {
+        assert.equal( catchError(NaN),isNaN(NaN));
+    });
+
+    it("если 2-й аргумент = isNaN возвращает true ", function() {
+        assert.equal( catchError(null, "csa"), isNaN("csa") );
     });
 });
 
-describe("compareNumb вычитает второй аргумент из первого", function(){
-    it("5-3=2", function() {
-        assert.equal(compareNumb(5, 3), 2);
+describe("outputValue", function() {
+
+    var storage = 4.55500;
+
+    function outputValue() {                        //видоизменённая функция (добавил return)
+        return output.value = parseFloat(storage);
+    }
+
+    it("Получая число и применяет parseFloat к нему ", function() {
+        assert.equal( outputValue(), 4.555 );
     });
 
-    it("-3-(-5)=2", function() {
-        assert.equal(compareNumb(-3, -5), 2);
+});
+
+describe("soundClick", function() {
+
+    var soundclick = true;
+
+    function soundClick() { //изменил функцию добавив return для проверки if
+        if (soundclick) {
+            beep.play();
+            return 1;
+        }
+    }
+
+    it("Получая true делает beep", function() {
+        assert.equal( soundClick(), 1 );
     });
+
 });
